98fceb089819bbd2d1ef594d76e0a8703608aeba
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-30 13:59:36 -0300

    [ :syringe: ] | update: fatory - server > cors (module::infra)

--- microsservice/src/modules/infra/server/server.factory.ts
+++ microsservice/src/modules/infra/server/server.factory.ts
@@ -1,8 +1,9 @@
 import { SERVER_MODULE } from './server.module';
 import { Express } from 'express';
 import { SERVER_REGISTRY } from './server.registry';
+import { CORS } from './http/cors';
 
 export const SERVER_FACTORY = {
   HTTP: () => SERVER_MODULE.get<Express>(SERVER_REGISTRY.HTTP),
-  CORS: () => SERVER_MODULE.get<any>(SERVER_REGISTRY.CORS),
+  CORS: () => SERVER_MODULE.get<typeof CORS>(SERVER_REGISTRY.CORS),
 };
