58ed041fc16d91d18cad8895d12b90865831c4f5
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-30 14:11:23 -0300

    [ :syringe: ] | update: factory - app > router (app::module)

--- microsservice/src/modules/app.factory.ts
+++ microsservice/src/modules/app.factory.ts
@@ -1,5 +1,7 @@
+import { ROUTER_FACTORY } from './application/router/router.factory';
 import { INFRA_FACTORY } from './infra/inra.factory';
 
 export const MODULES = {
   INFRA: INFRA_FACTORY,
+  ROUTER: ROUTER_FACTORY,
 };
