511b992d3d743909c0d62fcd2264e14c1ac2eb5a
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-30 14:19:51 -0300

    [ :sparkles: ] | setup: server (app)

--- microsservice/src/app.ts
+++ microsservice/src/app.ts
@@ -1,5 +1,8 @@
-import { MODULES } from 'modules/app.factory';
+import { MODULES } from './modules/app.factory';
 
 const app = MODULES.INFRA.SERVER.HTTP();
 
 app.use(MODULES.INFRA.SERVER.CORS());
+app.use(MODULES.ROUTER.APP());
+
+export { app };
