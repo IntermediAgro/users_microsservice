3867e7149aeb5fedde4a4f4466ebc4ed7da1cc0b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-30 14:22:27 -0300

    [ :syringe: ] | update: factory - server > parser (module::infra)

--- microsservice/src/modules/infra/server/server.factory.ts
+++ microsservice/src/modules/infra/server/server.factory.ts
@@ -2,8 +2,13 @@ import { SERVER_MODULE } from './server.module';
 import { Express } from 'express';
 import { SERVER_REGISTRY } from './server.registry';
 import { CORS } from './http/cors';
+import { BODY_PARSER } from './http/body.parser';
 
 export const SERVER_FACTORY = {
   HTTP: () => SERVER_MODULE.get<Express>(SERVER_REGISTRY.HTTP),
   CORS: () => SERVER_MODULE.get<typeof CORS>(SERVER_REGISTRY.CORS),
+  PARSER: {
+    BODY: () =>
+      SERVER_MODULE.get<typeof BODY_PARSER>(SERVER_REGISTRY.PARSER.BODY),
+  },
 };
