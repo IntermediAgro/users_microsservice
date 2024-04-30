cebf0c4c0d32e5f8be1635cd7897fa82b5ba48d4
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-30 13:57:03 -0300

    [ :syringe: ] | update: module - server > cors (module::infra)

--- microsservice/src/modules/infra/server/server.module.ts
+++ microsservice/src/modules/infra/server/server.module.ts
@@ -1,7 +1,9 @@
 import { Container } from 'inversify';
 import { SERVER_REGISTRY } from './server.registry';
 import { EXPRESS_SERVER } from './http/express.server';
+import { CORS } from './http/cors';
 
 export const SERVER_MODULE = new Container({ autoBindInjectable: true });
 
 SERVER_MODULE.bind(SERVER_REGISTRY.HTTP).toConstantValue(EXPRESS_SERVER);
+SERVER_MODULE.bind(SERVER_REGISTRY.CORS).toConstantValue(CORS);
