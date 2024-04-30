f318229beb414760e29e58b1237b49e444fc1d9e
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-30 14:20:47 -0300

    [ :syringe: ] | update: registry - server > parser (module::infra)

--- microsservice/src/modules/infra/server/server.registry.ts
+++ microsservice/src/modules/infra/server/server.registry.ts
@@ -1,4 +1,7 @@
 export const SERVER_REGISTRY = {
   HTTP: Symbol.for('MODULE::INFRA::SERVER::HTTP'),
   CORS: Symbol.for('MODULE::INFRA::SERVER::CORS'),
+  PARSER: {
+    BODY: Symbol.for('MODULE::INFRA::SERVER::PARSER::BODY'),
+  },
 };
