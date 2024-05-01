3a947cc5cf5336ba82b06ff2262252587a00e489
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-30 22:40:11 -0300

    [ :syringe: ] | update: module - app > repository (app::module)

--- microsservice/src/modules/app.module.ts
+++ microsservice/src/modules/app.module.ts
@@ -4,6 +4,7 @@ import { Container } from 'inversify';
 import { INFRA_MODULE } from './infra/infra.module';
 import { ROUTER_MODULE } from './application/router/router.module';
 import { MIDDLEWARE_MODULE } from './application/middleware/middleware.module';
+import { REPOSITORY_MODULE } from './application/repository/repository.module';
 
 const _MODULE = new Container({ autoBindInjectable: true });
 
@@ -12,4 +13,5 @@ export const APP_MODULE = Container.merge(
   INFRA_MODULE,
   ROUTER_MODULE,
   MIDDLEWARE_MODULE,
+  REPOSITORY_MODULE,
 );
