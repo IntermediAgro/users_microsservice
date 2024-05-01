700ed0d2b7ebe58041c57f5ebae97e429dd5c121
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-01 18:17:09 -0300

    [ :syringe: ] | update: factory - engine > jwt (module::infra)

--- microsservice/src/modules/infra/engine/engine.factory.ts
+++ microsservice/src/modules/infra/engine/engine.factory.ts
@@ -1,7 +1,9 @@
 import { PrismaClient } from '@prisma/client';
 import { ENGINE_MODULE } from './engine.module';
 import { ENGINE_REGISTRY } from './engine.registry';
+import { JWT_ENGINE } from './jwt/jwt.engine';
 
 export const ENGINE_FACTORY = {
   PRISMA: ENGINE_MODULE.get<PrismaClient>(ENGINE_REGISTRY.PRISMA),
+  JWT: ENGINE_MODULE.get<typeof JWT_ENGINE>(ENGINE_REGISTRY.JWT),
 };
