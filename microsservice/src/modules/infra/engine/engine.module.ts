e0e55855ebcf78761996e1cc10ea23c3f3ce7786
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-01 18:16:21 -0300

    [ :syringe: ] | update: module - engine > jwt (module::infra)

--- microsservice/src/modules/infra/engine/engine.module.ts
+++ microsservice/src/modules/infra/engine/engine.module.ts
@@ -1,6 +1,7 @@
 import { Container } from 'inversify';
 import { ENGINE_REGISTRY } from './engine.registry';
 import { PRISMA_ENGINE } from './prisma/prisma.engine';
+import { JWT_ENGINE } from './jwt/jwt.engine';
 
 export const ENGINE_MODULE = new Container({
   autoBindInjectable: true,
@@ -8,3 +9,4 @@ export const ENGINE_MODULE = new Container({
 });
 
 ENGINE_MODULE.bind(ENGINE_REGISTRY.PRISMA).toConstantValue(PRISMA_ENGINE);
+ENGINE_MODULE.bind(ENGINE_REGISTRY.JWT).toConstantValue(JWT_ENGINE);
