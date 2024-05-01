fe87736df8c161cad49bbb4a4ec674e22d0b80fa
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-01 09:43:48 -0300

    [ :syringe: ] | create: module - controller (module::application)

---
+++ microsservice/src/modules/application/controller/controller.module.ts
@@ -0,0 +1,13 @@
+import { SERVER_MODULE } from 'modules/infra/server/server.module';
+import { CONTROLLER_REGISTRY } from './controller.registry';
+import { UserController } from './user/user.controller';
+import { Container } from 'inversify';
+
+const MODULE = new Container({
+  autoBindInjectable: true,
+  defaultScope: 'Singleton',
+});
+
+export const CONTROLLER_MODULE = Container.merge(MODULE, SERVER_MODULE);
+
+CONTROLLER_MODULE.bind(CONTROLLER_REGISTRY.USER).to(UserController);
