d6b29eb8b31b0b81318abf3f63009dba5f5fc200
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-30 22:40:30 -0300

    [ :syringe: ] | update: factory - app > repository (app::module)

--- microsservice/src/modules/app.factory.ts
+++ microsservice/src/modules/app.factory.ts
@@ -1,4 +1,5 @@
 import { MIDDLEWARE_FACTORY } from './application/middleware/middleware.factory';
+import { REPOSITORY_FACTORY } from './application/repository/repository.factory';
 import { ROUTER_FACTORY } from './application/router/router.factory';
 import { INFRA_FACTORY } from './infra/inra.factory';
 
@@ -6,4 +7,5 @@ export const MODULES = {
   INFRA: INFRA_FACTORY,
   ROUTER: ROUTER_FACTORY,
   MIIDLEWARE: MIDDLEWARE_FACTORY,
+  REPOSITORY: REPOSITORY_FACTORY,
 };
