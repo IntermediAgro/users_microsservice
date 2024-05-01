7ba31345dabcd426c528e4b78514705b1daa56e7
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-01 09:40:42 -0300

    [ :sparkles: ] | update: controller - user (module::application)

--- microsservice/src/modules/application/controller/user/user.controller.ts
+++ microsservice/src/modules/application/controller/user/user.controller.ts
@@ -1,15 +1,16 @@
 import { injectable } from 'inversify';
 import { UserService } from '../../../../modules/application/service/user/user.service';
+import { ICreateUserDTO } from '../../../../modules/domain/dto/user/create.dto';
 
 @injectable()
 export class UserController {
   constructor(private readonly _service: UserService) {}
 
-  async create(user: any) {
-    return await this._service.create(user);
+  async create(user: ICreateUserDTO) {
+    return (await this._service.create(user)).toDTO();
   }
 
-  async findByEmail(user: any) {
-    return await this._service.findByEmail(user);
+  async findByEmail(user: ICreateUserDTO) {
+    return (await this._service.findByEmail(user)).toDTO();
   }
 }
