fe58d22b42454ae70a0a17606d63f416567dee46
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-01 08:08:49 -0300

    [ :sparkles: ] | implements: service - user (module::application)

--- microsservice/src/modules/application/service/user/user.service.ts
+++ microsservice/src/modules/application/service/user/user.service.ts
@@ -1,4 +1,7 @@
-import { injectable } from 'inversify';
+import { inject, injectable } from 'inversify';
+import { MODULE } from 'modules/app.registry';
+import { CreateUserUseCase } from 'modules/application/use_case/user/create.use_case';
+import { FindUserByEmailUseCase } from 'modules/application/use_case/user/find/by/email.use_case';
 import { ICreateUserDTO } from 'modules/domain/dto/user/create.dto';
 import { IFindUserByEmailDTO } from 'modules/domain/dto/user/find/by/email.dto';
 import { User } from 'modules/domain/entity/user.entity';
@@ -6,10 +9,17 @@ import { IUserService } from 'modules/domain/service/user/user.service';
 
 @injectable()
 export class UserService implements IUserService {
-  create(user: ICreateUserDTO): Promise<User> {
-    throw new Error('Method not implemented.');
+  constructor(
+    @inject(MODULE.USE_CASE.USER.CREATE)
+    private readonly _create: CreateUserUseCase,
+    @inject(MODULE.USE_CASE.USER.FIND.BY.EMAIL)
+    private readonly _findByEmail: FindUserByEmailUseCase,
+  ) {}
+
+  async create(user: ICreateUserDTO) {
+    return await this._create.execute(user);
   }
-  findByEmail(user: IFindUserByEmailDTO): Promise<User> {
-    throw new Error('Method not implemented.');
+  async findByEmail(user: IFindUserByEmailDTO) {
+    return await this._findByEmail.execute(user);
   }
 }
