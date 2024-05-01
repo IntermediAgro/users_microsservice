a3e8dccf2e4e39e883c1e2aac49ed24074723c2f
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-01 09:45:47 -0300

    [ :syringe: ] | update: module - app > service (app::module)

--- microsservice/src/modules/app.module.ts
+++ microsservice/src/modules/app.module.ts
@@ -6,6 +6,7 @@ import { ROUTER_MODULE } from './application/router/router.module';
 import { MIDDLEWARE_MODULE } from './application/middleware/middleware.module';
 import { REPOSITORY_MODULE } from './application/repository/repository.module';
 import { USE_CASE_MODULE } from './application/use_case/use_case.module';
+import { SERVICE_MODULE } from './application/service/service.module';
 
 const _MODULE = new Container({ autoBindInjectable: true });
 
@@ -16,4 +17,5 @@ export const APP_MODULE = Container.merge(
   MIDDLEWARE_MODULE,
   REPOSITORY_MODULE,
   USE_CASE_MODULE,
+  SERVICE_MODULE,
 );
