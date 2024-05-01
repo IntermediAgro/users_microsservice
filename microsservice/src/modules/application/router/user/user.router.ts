6d4a65194ef29e599daf64a9b9e0346f55794044
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-01 09:50:47 -0300

    [ :vertical_traffic_light: ] | create: router - user (app::module)

--- microsservice/src/modules/application/router/user/user.router.ts
+++ microsservice/src/modules/application/router/user/user.router.ts
@@ -1,6 +1,15 @@
 import { Router } from 'express';
+import { MODULES } from 'modules/app.factory';
 import { TRY } from 'modules/util/try.util';
 
 const USER_ROUTER = Router();
 
-USER_ROUTER.get('/', (req, res, next) => TRY(() => res.send(), next));
+USER_ROUTER.post('/', (req, res, next) =>
+  TRY(() => res.send(MODULES.CONTROLLER.USER().create(req.body)), next),
+);
+
+USER_ROUTER.get('/:email', (req, res, next) =>
+  TRY(() => res.send(MODULES.CONTROLLER.USER().findByEmail(req.params)), next),
+);
+
+export { USER_ROUTER };
