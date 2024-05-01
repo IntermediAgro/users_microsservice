4e297c57655637567201b7f938e65842b69c0a08
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-30 21:09:46 -0300

    [ :syringe: ] | update: factory - infra > engine (module::infra)

--- microsservice/src/modules/infra/inra.factory.ts
+++ microsservice/src/modules/infra/inra.factory.ts
@@ -1,5 +1,7 @@
+import { ENGINE_FACTORY } from './engine/engine.factory';
 import { SERVER_FACTORY } from './server/server.factory';
 
 export const INFRA_FACTORY = {
   SERVER: SERVER_FACTORY,
+  ENGINE: ENGINE_FACTORY,
 };
