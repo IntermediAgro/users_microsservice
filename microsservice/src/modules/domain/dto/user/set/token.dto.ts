a9c5c55fa7912314c9aa2b20dc2900c559f5ba8a
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-30 21:26:04 -0300

    [ :label: ] | create: DTO - set [token] > user (module::domain)

---
+++ microsservice/src/modules/domain/dto/user/set/token.dto.ts
@@ -0,0 +1,3 @@
+export interface ISetUserTokenDTO {
+  token: string;
+}
--- microsservice/src/modules/domain/dto/user/user.dto.ts
+++ microsservice/src/modules/domain/dto/user/user.dto.ts
@@ -2,6 +2,6 @@ export interface IUserDTO {
   name: string;
   email: string;
   password: string;
-  token?: string;
+  token?: string | null;
   user_type: 'CLIENT' | 'FARMER' | 'SUPPLIER';
 }
