37efcf6f1cb1245e2cd177e3f6db837c363582f6
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-01 09:45:31 -0300

    [ :syringe: ] | update: registry - app > service (app::module)

--- microsservice/src/modules/app.registry.ts
+++ microsservice/src/modules/app.registry.ts
@@ -1,6 +1,7 @@
 import { MIDDLEWARE_REGISTRY } from './application/middleware/middleware.registry';
 import { REPOSITORY_REGISTRY } from './application/repository/repository.registry';
 import { ROUTER_REGISTRY } from './application/router/router.registry';
+import { SERVICE_REGISTRY } from './application/service/service.registry';
 import { USE_CASE_REGISTRY } from './application/use_case/use_case.registry';
 import { INFRA_REGISTRY } from './infra/infra.registry';
 
@@ -10,4 +11,5 @@ export const MODULE = {
   MIIDLEWARE: MIDDLEWARE_REGISTRY,
   REPOSITORY: REPOSITORY_REGISTRY,
   USE_CASE: USE_CASE_REGISTRY,
+  SERVICE: SERVICE_REGISTRY,
 };
