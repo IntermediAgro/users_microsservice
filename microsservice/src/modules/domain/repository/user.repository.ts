4b95fd6d1de09fd4a4d760470a7cc4ff64df5ff0
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-30 21:45:00 -0300

    [ :card_file_box: ] | update: repository - user (module::domain)

--- microsservice/src/modules/domain/repository/user.repository.ts
+++ microsservice/src/modules/domain/repository/user.repository.ts
@@ -1,8 +1,10 @@
 import { ICreateUserDTO } from '../dto/user/create.dto';
 import { IFindUserByEmail } from '../dto/user/find/by/email.dto';
+import { ISetUserTokenDTO } from '../dto/user/set/token.dto';
 import { User } from '../entity/user.entity';
 
 export interface IUserRepository {
   create(user: ICreateUserDTO): Promise<User>;
   findByEmail(user: IFindUserByEmail): Promise<User>;
+  setToken(user: ISetUserTokenDTO): void;
 }
