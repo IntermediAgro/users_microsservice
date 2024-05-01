ac28271820e813f68795dcd5b36815b7aab063a5
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-01 08:06:53 -0300

    [ :syringe: ] | update: registry - app > use-case (app::module)

--- microsservice/src/modules/app.factory.ts
+++ microsservice/src/modules/app.factory.ts
@@ -1,6 +1,7 @@
 import { MIDDLEWARE_FACTORY } from './application/middleware/middleware.factory';
 import { REPOSITORY_FACTORY } from './application/repository/repository.factory';
 import { ROUTER_FACTORY } from './application/router/router.factory';
+import { USE_CASE_FACTORY } from './application/use_case/use_case.factory';
 import { INFRA_FACTORY } from './infra/inra.factory';
 
 export const MODULES = {
@@ -8,4 +9,5 @@ export const MODULES = {
   ROUTER: ROUTER_FACTORY,
   MIIDLEWARE: MIDDLEWARE_FACTORY,
   REPOSITORY: REPOSITORY_FACTORY,
+  USE_CASE: USE_CASE_FACTORY,
 };
