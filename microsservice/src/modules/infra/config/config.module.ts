5d60319f5bc093dd57c3543487abe17e6c66a697
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-01 21:10:56 -0300

    [ :syringe: ] | create: module - config (module::infra)

---
+++ microsservice/src/modules/infra/config/config.module.ts
@@ -0,0 +1,19 @@
+import { Container } from 'inversify';
+import { CONFIG_REGISTRY } from './config.registry';
+import { CONFIG } from './app.config';
+
+export const CONFIG_MODULE = new Container({
+  autoBindInjectable: true,
+  defaultScope: 'Singleton',
+});
+
+CONFIG_MODULE.bind(CONFIG_REGISTRY.CONFIG).toConstantValue(CONFIG);
+CONFIG_MODULE.bind(CONFIG_REGISTRY.ENV).toConstantValue(CONFIG.ENV);
+
+CONFIG_MODULE.bind(CONFIG_REGISTRY.DATABASE.URL).toConstantValue(
+  CONFIG.ENV.DATABASE.URL,
+);
+
+CONFIG_MODULE.bind(CONFIG_REGISTRY.JWT.SECRET).toConstantValue(
+  CONFIG.ENV.JWT.SECRET,
+);
