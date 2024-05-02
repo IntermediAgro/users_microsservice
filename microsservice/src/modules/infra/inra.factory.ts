1f64c42b45f35a09ab78f2f693f0593a9486de0a
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-01 21:21:00 -0300

    [ :syringe: ] | update: registry - config (module::infra)

--- microsservice/src/modules/infra/inra.factory.ts
+++ microsservice/src/modules/infra/inra.factory.ts
@@ -1,7 +1,9 @@
+import { CONFIG_FACTORY } from './config/config.factory';
 import { ENGINE_FACTORY } from './engine/engine.factory';
 import { SERVER_FACTORY } from './server/server.factory';
 
 export const INFRA_FACTORY = {
   SERVER: SERVER_FACTORY,
   ENGINE: ENGINE_FACTORY,
+  CONFIG: CONFIG_FACTORY,
 };
