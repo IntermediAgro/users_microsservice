82964e918dd8e2011abe9b4d52b27f5f17856e77
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-01 21:26:46 -0300

    [ :syringe: ] | update: registry - config (module::infra)

--- microsservice/src/modules/infra/infra.registry.ts
+++ microsservice/src/modules/infra/infra.registry.ts
@@ -1,7 +1,9 @@
+import { CONFIG_REGISTRY } from './config/config.registry';
 import { ENGINE_REGISTRY } from './engine/engine.registry';
 import { SERVER_REGISTRY } from './server/server.registry';
 
 export const INFRA_REGISTRY = {
   SERVER: SERVER_REGISTRY,
   EMGINE: ENGINE_REGISTRY,
+  CONFIG: CONFIG_REGISTRY,
 };
